9048d171357a83374077afd087b2036e4dfbb73f
Wim Bervoets <wim.bervoets@pxl.be>
2024-12-10 16:11:20 +0100

    Add project files.

---
+++ Klassement/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Klassement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom bij het Klassement Beheer Systeem!");
+            Console.WriteLine("Kies een optie uit het menu:");
+            Console.WriteLine();
+            Console.WriteLine("1.Toon het klassement");
+            Console.WriteLine("2.Zoek de score van een deelnemer");
+            Console.WriteLine("3.Pas de score van een deelnemer aan of voeg een nieuwe deelnemer toe");
+            Console.WriteLine("4.Toon de gemiddelde score");
+            Console.WriteLine("5.Toon de deelnemer met de hoogste score");
+            Console.WriteLine("6.Stop het programma");
+            Console.WriteLine();
+            Console.Write("Maak uw keuze: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+            }
+
+
+        }
+    }
+}
